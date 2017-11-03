4e040bdb4a98e34733950afb795f4bd27b4dcdc8
vpetermio <vpetermio@yahoo.com>
2017-11-03 10:54:58 +0100

    Added intent login and register

--- app/src/main/java/com/example/adam/manoauto/Login/Login.java
+++ app/src/main/java/com/example/adam/manoauto/Login/Login.java
@@ -1,7 +1,13 @@
 package com.example.adam.manoauto.Login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.adam.manoauto.MainActivity;
+import com.example.adam.manoauto.R;
+import com.example.adam.manoauto.Register.Register;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +15,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void loginClick(View v)
+    {
+        Intent intent=new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    public void registerClick(View v)
+    {
+        Intent intent=new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/adam/manoauto/Register/Register.java
+++ app/src/main/java/com/example/adam/manoauto/Register/Register.java
@@ -3,6 +3,8 @@ package com.example.adam.manoauto.Register;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.adam.manoauto.R;
+
 public class Register extends AppCompatActivity {
 
     @Override
