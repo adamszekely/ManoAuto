981df00bde99c98d7aacf2e8c2b79257eb0e57d8
adamszekely <szekelyadam93@gmail.com>
2017-11-10 08:34:52 +0100

    Model types added

--- app/src/main/java/com/example/adam/manoauto/SearchActivity.java
+++ app/src/main/java/com/example/adam/manoauto/SearchActivity.java
@@ -8,12 +8,30 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.adam.manoauto.SearchBrand.SearchBrandActivity;
+import com.example.adam.manoauto.SearchBrand.SearchBrandAdapter;
+import com.google.android.flexbox.AlignContent;
+import com.google.android.flexbox.AlignItems;
+import com.google.android.flexbox.AlignSelf;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayout;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class SearchActivity extends AppCompatActivity implements ShareActionProvider.OnShareTargetSelectedListener, NavigationView.OnNavigationItemSelectedListener {
 
@@ -50,6 +68,37 @@ public class SearchActivity extends AppCompatActivity implements ShareActionProv
         //Enable the drawer to open and close
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        final ArrayList<SearchBrandActivity> brandList = new ArrayList<SearchBrandActivity>();
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"BMW"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Mercedes"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ford"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ferrari"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"BMW"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Mercedes"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ford"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ferrari"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"BMW"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Mercedes"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ford"));
+        brandList.add(new SearchBrandActivity(R.drawable.close_x_image,"Ferrari"));
+
+
+       SearchBrandAdapter brandAdapter = new SearchBrandAdapter(brandList,this);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(brandAdapter);
+        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
+        layoutManager.setFlexDirection(FlexDirection.ROW);
+        layoutManager.setFlexWrap(FlexWrap.WRAP);
+        layoutManager.setAlignItems(AlignItems.FLEX_START);
+        layoutManager.setJustifyContent(JustifyContent.FLEX_START);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+
+       /* SearchBrandAdapter brandAdapter = new SearchBrandAdapter(this,brandList);
+        ListView listView = (ListView)findViewById(R.id.listViewOfBrand);
+        listView.setAdapter(brandAdapter);*/
     }
 
     @Override
