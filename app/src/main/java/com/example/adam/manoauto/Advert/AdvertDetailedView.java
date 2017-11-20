5f256be54e38663f3f8847340ccb50746ec4a039
BiancaSgondea <32032694+BiancaSgondea@users.noreply.github.com>
2017-11-20 20:32:05 +0100

    Car advert layout created

---
+++ app/src/main/java/com/example/adam/manoauto/Advert/AdvertDetailedView.java
@@ -0,0 +1,15 @@
+package com.example.adam.manoauto.Advert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adam.manoauto.R;
+
+public class AdvertDetailedView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_car_advert);
+    }
+}
